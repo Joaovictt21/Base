af059d4ecbce830cc634a207b456d6df9747e04f
João Victor Soares Lopes <victorlopes.jv30@gmail.com>
2023-03-21 11:21:51 -0300

    Adicionando arquivos

---
+++ index.js
@@ -0,0 +1,20 @@
+var idade,nome
+var valorUm = 3
+var valorDois = 4 
+
+idade = prompt("Insira sua idade")
+console.log(idade)
+nome = prompt("Digite seu nome")
+console.log(nome)
+
+var soma = valorUm + valorDois
+var subtracao =  valorUm - valorDois
+var multiplicacao = valorUm * valorDois
+var divisao = valorUm / valorDois
+var modulo = valorUm % valorDois
+
+console.log("Soma: ", soma)
+console.log("Subtração: ", subtracao)
+console.log("Divisão: ", divisao)
+console.log("Multiplicação:", multiplicacao)
+console.log("Módulo: ", modulo)
